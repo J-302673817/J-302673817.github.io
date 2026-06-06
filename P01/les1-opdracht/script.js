1a209dbec37804a5e26acd28f6bdb34208bf329c
J-302673817 <302673817@student.rocmondriaan.nl>
2026-06-06 16:43:08 +0200

    cleaning

--- P01/les1-opdracht/script.js
+++
@@ -1,7 +0,0 @@
-const productName = "Samsung  A145 S super ultra" //string
-const prijs= 1954.99 // number
-const aantal = 14 // number
-const opVoorraad = true // bool
-
-const totaalPrijs = prijs * aantal // totaalPrijs
-console.log(totaalPrijs);
